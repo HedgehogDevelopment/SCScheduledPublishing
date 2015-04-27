79935dc496b73e2d16a8cf767405db22aca50754
RosenPetrov <rosen.petrov@hotmail.com>
2015-04-27 14:12:42 +0300

    ScheduledPublishingCommand refactored. Added SMTP classes and Configuration + Settings classes

--- ScheduledPublishing/CustomScheduledTasks/ScheduledPublishingCommand.cs
+++ ScheduledPublishing/CustomScheduledTasks/ScheduledPublishingCommand.cs
@@ -1,13 +1,10 @@
-﻿using System.Collections.Generic;
-using Sitecore;
-using Sitecore.Data;
-using Sitecore.Data.Items;
+﻿using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
-using Sitecore.Publishing;
 using Sitecore.Tasks;
-using System;
 using System.Linq;
-using System.Net.Mail;
+using ScheduledPublishing.Models;
+using ScheduledPublishing.SMTP;
+using ScheduledPublishing.Utils;
 
 namespace ScheduledPublishing.CustomScheduledTasks
 {
@@ -16,152 +13,23 @@ namespace ScheduledPublishing.CustomScheduledTasks
     /// </summary>
     public class ScheduledPublishingCommand
     {
-        protected Database _database;
-
-        public void SchedulePublishingTask(Item[] itemArray, CommandItem commandItem, ScheduleItem scheduledItem)
+        public void ExecuteScheduledPublish(Item[] items, CommandItem command, ScheduleItem schedule)
         {
-            _database = Sitecore.Configuration.Factory.GetDatabase("master");
-            IEnumerable<Item> itemsToPublish = GetItemsToPublish();
-
-            foreach (var item in itemsToPublish)
+            if (items == null || items.Count() != 1)
             {
-                Log.Info("Custom scheduled for publish (2): " + item.Name, this);
-                if (scheduledItem != null)
-                {
-                    bool isUnpublish = item["Unpublish"] == 1.ToString();
-                    //// if the item has Publishing targets defined, use them and publish to all of them
-                    //if (!string.IsNullOrEmpty(item[FieldIDs.PublishingTargets]))
-                    //{
-                    //    publishingTargets = item[FieldIDs.PublishingTargets].Split('|');
-                    //}
-                    //// if the item has no Publishing targets specified, publish to all
-                    //else
-                    //{
-                    IEnumerable<Item> publishingTargets = GetPublishingTargets();
-                    //}
-                    if (publishingTargets.Any())
-                    {
-                        bool isSuccessful = PublishItemToTargets(item, publishingTargets, isUnpublish);
-                        Notify("PNGPublishing@png.com", item["CreatedByEmail"], isUnpublish, item, isSuccessful);
-                    }
-                }
-                else Log.Info("scheduled item null", this);
-            }
-        }
-
-        private IEnumerable<Item> GetPublishingTargets()
-        {
-            var publishingTargets = PublishManager.GetPublishingTargets(_database);
-            if (publishingTargets != null)
-            {
-                return publishingTargets;
+                Log.Info("Scheduled Publish Task didn't execute because of missing or invalid count of PublishOptions item", this);
+                return;
             }
 
-            Log.Info("No publishing targets found", this);
-            return Enumerable.Empty<Item>();
-        }
+            var scheduledPublishOptions = new ScheduledPublishOptions(items[0]);
+            var handle = ScheduledPublishManager.Publish(scheduledPublishOptions);
 
-        private IEnumerable<Item> GetItemsToPublish()
-        {
-            try
+            //TODO: if send email feature is activated. Not sure if this belongs here since publish is async
+            if (true)
             {
-                Item schedulesFolder = _database.GetItem(Utils.Constants.PUBLISH_OPTIONS_FOLDER_ID);
-                List<Item> itemsToPublish = new List<Item>();
-                foreach (Item schedule in schedulesFolder.Children)
-                {
-                    if (!string.IsNullOrEmpty(schedule["Schedule"]) && !string.IsNullOrEmpty(schedule["Items"]))
-                    {
-                        DateTime targetDate = DateUtil.IsoDateToDateTime(schedule["Schedule"].Split('|').First());
-                        if (DateTime.Compare(targetDate.AddHours(1), DateTime.Now) <= 0
-                            || DateTime.Compare(targetDate.AddHours(-1), DateTime.Now) <= 0) //TODO: values here are for testing purposes only
-                        {
-                            Item targetItem = _database.GetItem(schedule["Items"]);
-                            itemsToPublish.Add(targetItem);
-                        }
-                    }
-                }
-
-                return itemsToPublish;
-            }
-            catch (Exception e)
-            {
-                Log.Info(e.ToString(), this);
-            }
-
-            return Enumerable.Empty<Item>();
-        }
-
-        private bool PublishItemToTargets(Item item, IEnumerable<Item> publishingTargets, bool isUnpublish)
-        {
-            bool successful = false;
-            foreach (var pbTarget in publishingTargets)
-            {
-                try
-                {
-                    if (isUnpublish)
-                    {
-                        item.Editing.BeginEdit();
-                        item["__Never publish"] = 1.ToString();
-                        item.Editing.AcceptChanges();
-                        item.Editing.EndEdit();
-                    }
-
-                    PublishOptions publishOptions = new PublishOptions(
-                        _database,
-                        Database.GetDatabase(pbTarget["Target database"]),
-                        PublishMode.SingleItem,
-                        item.Language,
-                        DateTime.Now);
-                    Sitecore.Publishing.Pipelines.PublishItem.PublishItemPipeline.Run(item.ID, publishOptions);
-
-                    Log.Info(
-                        "Scheduled publishing task complete for " + item.Name + " - " + item.ID
-                        + " Database source: " + _database.Name + " Database target: " +
-                        Database.GetDatabase(pbTarget["Target database"]).Name, this);
-
-                    if (isUnpublish)
-                    {
-                        item.Editing.BeginEdit();
-                        item["__Never publish"] = string.Empty;
-                        item.Editing.AcceptChanges();
-                        item.Editing.EndEdit();
-                    }
-
-                    successful = true;
-                }
-                catch (Exception e)
-                {
-                    Log.Info("Scheduled publishing task failed for " + item.Name + " - " + item.ID, this);
-                    Log.Info(e.ToString(), this);
-                    successful = false;
-                }
-            }
-
-            return successful;
-        }
-
-        public void Notify(string emailFrom, string emailTo, bool isUnpublish, Item item, bool success)
-        {
-            string action = isUnpublish ? "Unpublishing" : "Publishing";
-            string body = success
-                ? action + " {0} ({1}) completed successfully at {2}."
-                : action + " {0} ({1}) failed at {2}. Please restart publishing process.";
-
-            var smtpClient = new SmtpClient();
-            var mailMessage = new MailMessage(emailFrom, emailTo)
-            {
-                Subject = "PNG Publishing",
-                IsBodyHtml = true,
-                Body = string.Format(body, item.Name, item.Paths.FullPath, DateTime.Now),
-            };
-
-            try
-            {
-                smtpClient.Send(mailMessage);
-            }
-            catch (Exception e)
-            {
-                Log.Info("Sending email failed: " + e.ToString(), this);
+                var report = ScheduledPublishManager.GetPublishReport(handle);
+                var sendTo = scheduledPublishOptions.SchedulerEmail;
+                NotificationEmail.SendEmail(report, sendTo);
             }
         }
     }
---
+++ ScheduledPublishing/SMTP/NotificationEmail.cs
@@ -0,0 +1,12 @@
+﻿namespace ScheduledPublishing.SMTP
+{
+    public static class NotificationEmail
+    {
+        //TODO load other mail settings here
+
+        public static void SendEmail(string report, string sendTo)
+        {
+            //TODO Note: add sendTo in bcc 
+        }
+    }
+}
