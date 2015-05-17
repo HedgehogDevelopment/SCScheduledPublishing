c3d52a9138ef55114af41f724c891c9df77baa46
RosenPetrov <rosen.petrov@hotmail.com>
2015-05-17 23:28:46 +0300

    Version 7.0 - 7-2 separated

---
+++ Source/ScheduledPublish70-72/ScheduledPublish/Commands/OpenEditScheduledPublishDialog.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore;
+using Sitecore.Diagnostics;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Text;
+using Sitecore.Web.UI.Sheer;
+using System.Collections.Specialized;
+
+namespace ScheduledPublish.Commands
+{
+    public class OpenEditScheduledPublishDialog : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            Context.ClientPage.Start(this, "Run", new NameValueCollection());
+        }
+
+        protected void Run(ClientPipelineArgs args)
+        {
+            if (!SheerResponse.CheckModified())
+            {
+                return;
+            }
+                
+            if (args.IsPostBack)
+            {
+                return;
+            }
+
+            UrlString urlString = new UrlString(UIUtil.GetUri("control:EditScheduledPublish"));
+            SheerResponse.ShowModalDialog(urlString.ToString(), "700", "600", string.Empty, true);
+            args.WaitForPostBack();
+        }
+    }
+}
