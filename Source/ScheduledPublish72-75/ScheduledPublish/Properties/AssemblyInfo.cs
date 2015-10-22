e40b3826cfdff2464ba4b37c7f8923301bd6cc6c
Svetlana <spareva@abv.bg>
2015-10-22 16:20:35 +0300

    added assemblyinfo

--- Source/ScheduledPublish72-75/ScheduledPublish/Properties/AssemblyInfo.cs
+++ Source/ScheduledPublish72-75/ScheduledPublish/Properties/AssemblyInfo.cs
@@ -1,4 +1,29 @@
-﻿using System.Reflection;
+﻿#region License
+// Copyright © 2012 Hedgehog Development, LLC (www.hhogdev.com)
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +31,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ScheduledPublish")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/HedgehogDevelopment/SCScheduledPublishing")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Hedgehog Development, LLC http://www.hhogdev.com")]
 [assembly: AssemblyProduct("ScheduledPublish")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015 Hedgehog Development, LLC (http://www.hhogdev.com)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
